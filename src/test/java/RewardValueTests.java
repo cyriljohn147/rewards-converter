8c0890d984262d4765460982e4cd8072e555cbfe
cyriljohn147 <cyriljohn142005@gmail.com>
2024-08-21 22:09:25 +0530

    Task Two Commit

--- src/test/java/RewardValueTests.java
+++ src/test/java/RewardValueTests.java
@@ -1,30 +1,35 @@
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class RewardValueTests {
 
-    @Test
-    void create_with_cash_value() {
-        double cashValue = 100;
-        var rewardValue = new RewardValue(cashValue);
-        assertEquals(cashValue, rewardValue.getCashValue());
-    }
+   @Test
+   void create_with_cash_value() {
+       double cashValue = 100;
+       var rewardValue = new RewardValue(cashValue);
+       assertEquals(cashValue, rewardValue.getCashValue());
+   }
 
-    @Test
-    void create_with_miles_value() {
-        int milesValue = 10000;
-        var rewardValue = new RewardValue(milesValue);
-        assertEquals(milesValue, rewardValue.getMilesValue());
-    }
+   @Test
+   void create_with_miles_value() {
+       int milesValue = 10000;
+       var rewardValue = new RewardValue(milesValue);
+       assertEquals(milesValue, rewardValue.getMilesValue());
+   }
 
-    @Test
-    void convert_from_cash_to_miles() {
-        assert false;
-    }
+   @Test
+   void convert_from_cash_to_miles() {
+       double cashValue = 100;
+       int expectedMiles = (int) (cashValue / 0.0035);
+       var rewardValue = new RewardValue(cashValue);
+       assertEquals(expectedMiles, rewardValue.getMilesValue(), "Cash to miles conversion failed");
+   }
 
-    @Test
-    void convert_from_miles_to_cash() {
-        assert false;
-    }
+   @Test
+   void convert_from_miles_to_cash() {
+       int milesValue = 10000;
+       double expectedCash = milesValue * 0.0035;
+       var rewardValue = new RewardValue(milesValue);
+       assertEquals(expectedCash, rewardValue.getCashValue(), 0.0001, "Miles to cash conversion failed");
+   }
 }
